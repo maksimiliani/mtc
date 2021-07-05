7b98d54d6b5a1983646c5ce3d5e262cc0862261e
maksimiliani <macseg@gmail.com>
2021-07-06 00:01:02 +0300

    Update form-to-db.js

--- js/form-to-db.js
+++ js/form-to-db.js
@@ -8,12 +8,20 @@ $(document).ready( function() {
   form_submission.state="";
   form_submission.campaignId="68969";
   form_submission.vendorId="6000";
-  var jsonString;
+  //var jsonString;
 
   $('#contact_form').on("submit",function(){
     form_submission.url = window.location.href;
     form_submission.email = document.getElementById("Email").value;
-    jsonString = JSON.stringify(form_submission);
-    console.log(form_submission);
+    //jsonString = JSON.stringify(form_submission);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'http://service.topbutton.com/landing/register', true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+          console.console.log("form_submitted");
+      }
+    }
+    xhr.send(form_submission);
   });
 });
