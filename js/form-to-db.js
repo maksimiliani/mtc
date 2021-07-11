1039404bdffe37f8c82bf7b9000be63ba486341e
maksimiliani <40344223+maksimiliani@users.noreply.github.com>
2021-07-11 19:07:52 +0300

    Update form-to-db.js

--- js/form-to-db.js
+++ js/form-to-db.js
@@ -1,27 +1,25 @@
-$(document).ready( function() {
-  var form_submission = new Object();
-  form_submission.url="";
-  form_submission.email="";
-  form_submission.name="";
-  form_submission.mobile="";
-  form_submission.city="";
-  form_submission.state="";
-  form_submission.campaignId="68969";
-  form_submission.vendorId="6000";
-  var jsonString;
-
-  $('#contact_form').on("submit",function(){
+$('#contact_form').on("submit", function () {
+    var form_submission = new Object();
     form_submission.url = window.location.href;
     form_submission.email = document.getElementById("Email").value;
+    form_submission.name="";
+    form_submission.mobile="";
+    form_submission.city="";
+    form_submission.state="";
+    form_submission.campaignId = "68969";
+    form_submission.vendorId = "6000";
+    var jsonString;
+
     jsonString = JSON.stringify(form_submission);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", 'http://service.topbutton.com/landing/register', true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-          console.log("form_submitted");
-      }
-    }
-    xhr.send(jsonString);
-  });
+    //console.log(jsonString);
+
+    $.ajax({
+        type: "POST",
+        url: 'http://service.topbutton.com/landing/register',
+        data: jsonString,
+        headers: {
+            "Content-type": "application/json",
+        }
+
+    });
 });
