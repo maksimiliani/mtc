73dc3a48be55b9c34e8821b019db7d4f7af3a2e4
maksimiliani <macseg@gmail.com>
2021-07-22 22:18:17 +0300

    json upd

--- js/form-to-db.js
+++ js/form-to-db.js
@@ -1,4 +1,17 @@
 $(document).ready( function() {
+
+  function json_submission(jsonString_var) {
+    $.ajax({
+        type: "POST",
+        url: 'https://service.topbutton.com/landing/register',
+        crossDomain: true,
+        data: jsonString_var,
+        headers: {
+            "Content-type": "text/plain",
+        }
+    });
+  }
+
   $('#contact_form').on("submit",function(){
     var form_submission = new Object();
     form_submission.url = window.location.href;
@@ -9,20 +22,31 @@ $(document).ready( function() {
     form_submission.state="";
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
-    var jsonString;
-
-    jsonString = JSON.stringify(form_submission);
-    console.log(jsonString);
-
-    $.ajax({
-        type: "POST",
-        url: 'https://service.topbutton.com/landing/register',
-        crossDomain: true,
-        data: jsonString,
-        headers: {
-            "Content-type": "application/json",
-        }
-    });
+    json_submission(JSON.stringify(form_submission));
+  });
+  $('#conjured_save_advocate').on("submit",function(){
+    var form_submission = new Object();
+    form_submission.url = window.location.href;
+    form_submission.email = document.getElementById("conjured_referral_name").value;
+    form_submission.name = document.getElementById("conjured_referral_email").value;
+    form_submission.mobile="";
+    form_submission.city="";
+    form_submission.state="";
+    form_submission.campaignId = "68969";
+    form_submission.vendorId = "6000";
+    json_submission(JSON.stringify(form_submission));
+  });
+  $('#conjured_send_email_button').on("submit",function(){
+    var form_submission = new Object();
+    form_submission.url = window.location.href;
+    form_submission.email = document.getElementById("conjured_emails").value;
+    form_submission.name="";
+    form_submission.mobile="";
+    form_submission.city="";
+    form_submission.state="";
+    form_submission.campaignId = "68969";
+    form_submission.vendorId = "6000";
+    json_submission(JSON.stringify(form_submission));
   });
 });
 
