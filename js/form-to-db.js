bac7110d5a120c4e44c2f520aef67d5a25c0b431
maksimiliani <macseg@gmail.com>
2021-07-27 18:13:27 +0300

    Update form-to-db.js

--- js/form-to-db.js
+++ js/form-to-db.js
@@ -10,7 +10,7 @@ $(document).ready( function() {
     });
   }
 
-  $('#contact_form').on("submit",function(){
+  $('#contact_form .btn-success').click(function() {
     var form_submission = new Object();
     form_submission.url = window.location.href;
     form_submission.email = document.getElementById("Email").value;
@@ -20,9 +20,10 @@ $(document).ready( function() {
     form_submission.state="";
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
+    $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.vendorId = e.ip; });
     json_submission(JSON.stringify(form_submission));
   });
-  $('#conjured_save_advocate').on("submit",function(){
+  $( "#conjured_save_advocate" ).click(function() {
     var form_submission = new Object();
     form_submission.url = window.location.href;
     form_submission.email = document.getElementById("conjured_referral_name").value;
@@ -32,9 +33,10 @@ $(document).ready( function() {
     form_submission.state="";
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
+    $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.vendorId = e.ip; });
     json_submission(JSON.stringify(form_submission));
   });
-  $('#conjured_send_email_button').on("submit",function(){
+  $( "#conjured_send_email_button" ).click(function() {
     var form_submission = new Object();
     form_submission.url = window.location.href;
     form_submission.email = document.getElementById("conjured_emails").value;
@@ -44,32 +46,7 @@ $(document).ready( function() {
     form_submission.state="";
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
+    $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.vendorId = e.ip; });
     json_submission(JSON.stringify(form_submission));
   });
 });
-
-// $('#contact_form').on("submit", function () {
-//     var form_submission = new Object();
-//     form_submission.url = window.location.href;
-//     form_submission.email = document.getElementById("Email").value;
-//     form_submission.name="";
-//     form_submission.mobile="";
-//     form_submission.city="";
-//     form_submission.state="";
-//     form_submission.campaignId = "68969";
-//     form_submission.vendorId = "6000";
-//     var jsonString;
-//
-//     jsonString = JSON.stringify(form_submission);
-//     console.log(jsonString);
-//
-//     $.ajax({
-//         type: "POST",
-//         url: 'http://service.topbutton.com/landing/register',
-//         data: jsonString,
-//         headers: {
-//             "Content-type": "application/json",
-//         }
-//
-//     });
-// });
