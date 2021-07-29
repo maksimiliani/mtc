187630984ccc82c526e7d5f518701b3971feae5c
AlexT <at.dev@outlook.com>
2021-07-30 02:42:42 +0300

    fixed CORS

--- js/form-to-db.js
+++ js/form-to-db.js
@@ -3,59 +3,23 @@ $(document).ready( function() {
     $.ajax({
         type: "POST",
         url: 'https://service.massagetherapyconcepts.com:8443/landing/register',
-        //crossDomain: true,
+        crossDomain: true,
         data: jsonString_var,
         dataType : 'json',
-        contentType: "application/json;charset=utf-8"
+        contentType: "text/plain"
     });
   }
-
-  function json_submission2(jsonString_var) {
-    $.support.cors = true;
-    $.ajax({
-          type: "POST",
-          url: 'https://service.massagetherapyconcepts.com:8443/landing/register',
-          //crossDomain: true,
-          data: jsonString_var,
-          dataType : 'json',
-          accept: "application/json",
-          contentType: "application/json;charset=utf-8",
-  		success: function (data, text) {
-          var result = 'ok';
-          // Console.log(status);
-  		},
-  		error: function (xhr, status, error) {
-        var result = 'error';
-          console.log(error);
-          console.log(error);         
-  		}
-      });
-
-      console.log('error'); 
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', 'https://service.massagetherapyconcepts.com:8443/landing/register');
-      xhr.setRequestHeader('Content-type','application/json');
-      xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
-        }
-      };
-      xhr.onerror = function() {
-        console.log('There was an error!');
-      };
-      xhr.send(jsonString_var);
-  }
-
-
 
   $('#contact_form .btn-success').click(function() {
     var form_submission = new Object();
     form_submission.url = window.location.href;
+    form_submission.email = document.getElementById("Email").value;
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
     $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.ip = e.ip; });
-    json_submission2(JSON.stringify(form_submission));
+    json_submission(JSON.stringify(form_submission));
   });
+
   $( "#conjured_save_advocate" ).click(function() {
     var form_submission = new Object();
     form_submission.url = window.location.href;
@@ -64,8 +28,9 @@ $(document).ready( function() {
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
     $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.ip = e.ip; });
-    json_submission2(JSON.stringify(form_submission));
+    json_submission(JSON.stringify(form_submission));
   });
+  
   $( "#conjured_send_email_button" ).click(function() {
     var form_submission = new Object();
     form_submission.url = window.location.href;
@@ -73,6 +38,7 @@ $(document).ready( function() {
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
     $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.ip = e.ip; });
-    json_submission2(JSON.stringify(form_submission));
+    json_submission(JSON.stringify(form_submission));
   });
 });
+
