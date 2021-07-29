9aa3a03f872271a1922474284685b3df59a42054
AlexT <at.dev@outlook.com>
2021-07-29 13:57:19 +0300

    latest script changes

--- js/form-to-db.js
+++ js/form-to-db.js
@@ -19,12 +19,15 @@ $(document).ready( function() {
           dataType : 'json',
           contentType: "text/plain",
   		success: function (data, text) {
-          alert('ok');
+          var result = 'ok';
+          // Console.log(status);
   		},
-  		error: function (request, status, error) {
-  			alert(request.responseText);
-  			alert(status);
-  			alert(error);
+  		error: function (xhr, status, error) {
+        var result = 'error';
+         Console.log(xhr.status);
+         Console.log(xhr.responseText);
+         var errorMessage = xhr.status + ': ' + xhr.statusText;
+         alert('Error - ' + errorMessage);
   		}
       });
   }
@@ -34,14 +37,10 @@ $(document).ready( function() {
   $('#contact_form .btn-success').click(function() {
     var form_submission = new Object();
     form_submission.url = window.location.href;
-    form_submission.email = document.getElementById("Email").value;
-    form_submission.name="";
-    form_submission.mobile="";
-    form_submission.city="";
-    form_submission.state="";
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
-    $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.vendorId = e.ip; });
+    form_submission.ip = "6000";
+    $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.ip = e.ip; });
     json_submission2(JSON.stringify(form_submission));
   });
   $( "#conjured_save_advocate" ).click(function() {
@@ -49,25 +48,18 @@ $(document).ready( function() {
     form_submission.url = window.location.href;
     form_submission.email = document.getElementById("conjured_referral_name").value;
     form_submission.name = document.getElementById("conjured_referral_email").value;
-    form_submission.mobile="";
-    form_submission.city="";
-    form_submission.state="";
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
-    $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.vendorId = e.ip; });
-    json_submission(JSON.stringify(form_submission));
+    $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.ip = e.ip; });
+    json_submission2(JSON.stringify(form_submission));
   });
   $( "#conjured_send_email_button" ).click(function() {
     var form_submission = new Object();
     form_submission.url = window.location.href;
     form_submission.email = document.getElementById("conjured_emails").value;
-    form_submission.name="";
-    form_submission.mobile="";
-    form_submission.city="";
-    form_submission.state="";
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
-    $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.vendorId = e.ip; });
-    json_submission(JSON.stringify(form_submission));
+    $.getJSON("https://api.ipify.org/?format=json", function(e) { form_submission.ip = e.ip; });
+    json_submission2(JSON.stringify(form_submission));
   });
 });
