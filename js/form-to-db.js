eaa5c5ec5f9553f4d0a39384224d3e82af83da41
AlexT <at.dev@outlook.com>
2021-09-01 22:04:32 +0300

    updated script

--- js/form-to-db.js
+++ js/form-to-db.js
@@ -1,40 +1,46 @@
 $(document).ready( function() {
-  function json_submission(jsonString_var) {
+  function json_submission(jsonString_var, formName) {
     $.ajax({
         type: "POST",
         url: 'https://service.massagetherapyconcepts.com:8443/landing/register',
         crossDomain: true,
         data: jsonString_var,
         dataType : 'json',
-        contentType: "text/plain"
+        contentType: "text/plain",
+        complete: function (result) {
+            $(formName).submit();
+        }
     });
   }
 
-  $('#contact_form').submit(function() {
+    $('#contact_form .btn-success').click(function (e) {
+    e.preventDefault();
     var form_submission = new Object();
     form_submission.url = window.location.href;
     form_submission.email = document.getElementById("Email").value;
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
-    json_submission(JSON.stringify(form_submission));
+    json_submission(JSON.stringify(form_submission), '#contact_form');
   });
 
-  $( "#conjured_save_advocate" ).click(function() {
+    $("#conjured_save_advocate").click(function (e) {
+     e.preventDefault();
     var form_submission = new Object();
     form_submission.url = window.location.href;
     form_submission.email = document.getElementById("conjured_referral_name").value;
     form_submission.name = document.getElementById("conjured_referral_email").value;
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
-    json_submission(JSON.stringify(form_submission));
+    json_submission(JSON.stringify(form_submission), '#conjured_save_advocate');
   });
 
-  $( "#conjured_send_email_button" ).click(function() {
+    $("#conjured_send_email_button").click(function (e) {
+    e.preventDefault();
     var form_submission = new Object();
     form_submission.url = window.location.href;
     form_submission.email = document.getElementById("conjured_emails").value;
     form_submission.campaignId = "68969";
     form_submission.vendorId = "6000";
-    json_submission(JSON.stringify(form_submission));
+    json_submission(JSON.stringify(form_submission), "#conjured_send_email_button");
   });
 });
