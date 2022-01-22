fb42031bb45cd88928861489b9562155443c40e9
AlexT <at.dev@outlook.com>
2022-01-22 19:27:08 +0300

    fixed function name

--- js/form-to-db.js
+++ js/form-to-db.js
@@ -1,5 +1,5 @@
-$(document).ready( function() {
-  function json_submission(jsonString_var, formName) {
+$(document).ready(function () {
+    function mtc_form_submit(jsonString_var, formName) {
     $.ajax({
         type: "POST",
         url: 'https://service.massagetherapyconcepts.com:8443/landing/register',
@@ -16,43 +16,43 @@ $(document).ready( function() {
 
     $('#contact_form .btn-success').click(function (e) {
     e.preventDefault();
-    var form_submission = new Object();
-    form_submission.url = window.location.href;
-    form_submission.email = document.getElementById("Email").value;
-    form_submission.campaignId = "68969";
-    form_submission.vendorId = "6000";
-    json_submission(JSON.stringify(form_submission), '#contact_form');
+    var mtc_data_form = new Object();
+    mtc_data_form.url = window.location.href;
+    mtc_data_form.email = document.getElementById("Email").value;
+    mtc_data_form.campaignId = "68969";
+    mtc_data_form.vendorId = "6000";
+    mtc_form_submit(JSON.stringify(mtc_data_form), '#contact_form');
   });
 
     $("#conjured_save_advocate").click(function (e) {
      e.preventDefault();
-    var form_submission = new Object();
-    form_submission.url = window.location.href;
-    form_submission.email = document.getElementById("conjured_referral_name").value;
-    form_submission.name = document.getElementById("conjured_referral_email").value;
-    form_submission.campaignId = "68969";
-    form_submission.vendorId = "6000";
-    json_submission(JSON.stringify(form_submission), '#conjured_save_advocate');
+    var mtc_data_form = new Object();
+    mtc_data_form.url = window.location.href;
+    mtc_data_form.email = document.getElementById("conjured_referral_name").value;
+    mtc_data_form.name = document.getElementById("conjured_referral_email").value;
+    mtc_data_form.campaignId = "68969";
+    mtc_data_form.vendorId = "6000";
+    mtc_form_submit(JSON.stringify(mtc_data_form), '#conjured_save_advocate');
   });
 
     $("#conjured_send_email_button").click(function (e) {
     e.preventDefault();
-    var form_submission = new Object();
-    form_submission.url = window.location.href;
-    form_submission.email = document.getElementById("conjured_emails").value;
-    form_submission.campaignId = "68969";
-    form_submission.vendorId = "6000";
-    json_submission(JSON.stringify(form_submission), "#conjured_send_email_button");
+    var mtc_data_form = new Object();
+    mtc_data_form.url = window.location.href;
+    mtc_data_form.email = document.getElementById("conjured_emails").value;
+    mtc_data_form.campaignId = "68969";
+    mtc_data_form.vendorId = "6000";
+    mtc_form_submit(JSON.stringify(mtc_data_form), "#conjured_send_email_button");
     });
 
     $('.shogun-form-box-submit').click(function (e) {
-        var form_submission = new Object();
-        form_submission.url = window.location.href;
-        form_submission.email = $('.shogun-form-box input[placeholder="Email"]').val();
-        form_submission.name = $('.shogun-form-box input[placeholder="Name"]').val();
-        form_submission.campaignId = "68969";
-        form_submission.vendorId = "6000";
-        json_submission(JSON.stringify(form_submission), '');
+        var mtc_data_form = new Object();
+        mtc_data_form.url = window.location.href;
+        mtc_data_form.email = $('.shogun-form-box input[placeholder="Email"]').val();
+        mtc_data_form.name = $('.shogun-form-box input[placeholder="Name"]').val();
+        mtc_data_form.campaignId = "68969";
+        mtc_data_form.vendorId = "6000";
+        mtc_form_submit(JSON.stringify(mtc_data_form), '');
     });
 
 });
