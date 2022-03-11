ca1440c7031ef758dba917bcf60929d54adba86d
AlexT <at.dev@outlook.com>
2022-03-11 20:39:55 +0300

    fixes

--- js/form-to-db.js
+++ js/form-to-db.js
@@ -34,8 +34,7 @@ $(document).ready(function () {
         var mtc_data_form = new Object();
         mtc_data_form.url = window.location.href;
         mtc_data_form.email = $("input[placeholder='Email']").val();
-        mtc_data_form.firstName = $("input[placeholder='First name']").val();
-        mtc_data_form.lastName = $("input[placeholder='Last name']").val();
+        mtc_data_form.firstName = $("input[placeholder='First and Last name']").val();
         mtc_data_form.mobile = $("input[placeholder='Mobile Phone']").val();
         mtc_data_form.address1 = $("input[placeholder='Address']").val();
         mtc_data_form.address2 = $("input[placeholder='Address 2']").val();
@@ -68,7 +67,21 @@ $(document).ready(function () {
         mtc_data_form.campaignId = "6000";
         mtc_data_form.vendorId = "6000";
         mtc_form_submit(JSON.stringify(mtc_data_form), null);
-  });
+    });
+
+    /// spring offer popup
+    $("form.proof-factor-cb-prompt-form").submit(function (e) {
+        var mtc_data_form = new Object();
+        mtc_data_form.url = window.location.href;
+        
+        mtc_data_form.email = $('input#proof-factor-email-input-bcb').val();
+        mtc_data_form.firstName = $("input.proof-factor-cb-prompt-input-firstname").val();
+        mtc_data_form.lastName = $("input.proof-factor-cb-prompt-input-lastname").val();
+
+        mtc_data_form.campaignId = "6000";
+        mtc_data_form.vendorId = "6000";
+        mtc_form_submit(JSON.stringify(mtc_data_form), null);
+    });
 
     //$("#conjured_send_email_button").click(function (e) {
     //    e.preventDefault();
@@ -80,22 +93,22 @@ $(document).ready(function () {
     //    mtc_form_submit(JSON.stringify(mtc_data_form), "#conjured_send_email_button");
     //});
 
-    $('.shogun-form-box-submit').click(function (e) {
-        var mtc_data_form = new Object();
-        mtc_data_form.url = window.location.href;
-        mtc_data_form.email = $('.shogun-form-box input[placeholder="Email"]').val();
-        mtc_data_form.name = $('.shogun-form-box input[placeholder="Name"]').val();
-        mtc_data_form.campaignId = "6000";
-        mtc_data_form.vendorId = "6000";
-        mtc_form_submit(JSON.stringify(mtc_data_form), null)
-    });
+    //$('.shogun-form-box-submit').click(function (e) {
+    //    var mtc_data_form = new Object();
+    //    mtc_data_form.url = window.location.href;
+    //    mtc_data_form.email = $('.shogun-form-box input[placeholder="Email"]').val();
+    //    mtc_data_form.name = $('.shogun-form-box input[placeholder="Name"]').val();
+    //    mtc_data_form.campaignId = "6000";
+    //    mtc_data_form.vendorId = "6000";
+    //    mtc_form_submit(JSON.stringify(mtc_data_form), null)
+    //});
 
     /// footer
     $('#footer_email_form .btn-success').click(function (e) {       //   $('#contact_form .btn-success') -  jQuery request to select submit button.
         e.preventDefault();
         var mtc_data_form = new Object();
         mtc_data_form.url = window.location.href;
-        mtc_data_form.email = $("Email").val();                         // Obtain email value from element with ID "Email"
+        mtc_data_form.email = $("input#Email.newsletter").val();                         // Obtain email value from element with ID "Email"
         mtc_data_form.campaignId = "6000";                              // hardcoded value campaignId = "68969";
         mtc_data_form.vendorId = "6000";                                 // mtc_data_form.vendorId = "6000";
         mtc_form_submit(JSON.stringify(mtc_data_form), '#footer_email_form'); //  JSON.stringify(mtc_data_form) convert object "mtc_data_form" to string; call mtc_form_submit to submit data.
